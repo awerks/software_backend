9a98efdae2645d1fb95ef1edd3ba4f7f0328a9d5
zynpyrtsvr <zeynepyurtsever23@gmail.com>
2025-04-10 20:43:42 +0200

    Create getters & setters for Project

--- src/main/java/com/seproject/backend/entity/Project.java
+++ src/main/java/com/seproject/backend/entity/Project.java
@@ -1,8 +1,12 @@
 package com.seproject.backend.entity;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
 
 @Entity
 @Table(name = "projects")
@@ -11,7 +15,45 @@ public class Project {
     @Column(name = "project_id", nullable = false)
     private Integer projectId;
 
+    @Column(name = "project_name", nullable = false)
+    private String projectName;
+
+    @Column
+    @Temporal(TemporalType.TIMESTAMP) // TODO: check the type
+    private LocalDateTime creationDate;
+
+    @Column
+    private String description;
+
     public Integer getProjectId() {
         return projectId;
+    }
+
+    public void setProjectId(Integer projectId) {
+        this.projectId = projectId;
+    } 
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public void setProjectName(String projectName) {
+        this.projectName = projectName;
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
     }
 }
